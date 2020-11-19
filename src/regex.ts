5406453dbd7898d8d4f6cda69e27a564a90255c1
kellertk <kellertk@amazon.com>
2020-11-19 14:31:44 -0800

    Add extra debugs

--- src/regex.ts
+++ src/regex.ts
@@ -1,11 +1,17 @@
+import * as core from '@actions/core';
+
 function isJsonArray(token: string): boolean {
+  core.debug(`Your token is ${token}`);
   try {
     if (Array.isArray(JSON.parse(token))) {
+      core.debug('Is JSON arrray');
       return true;
     }
   } catch (err) {
+    core.debug(`Isn't JSON array: ${err}`);
     return false;
   }
+  core.debug('Is JSON but not array');
   return false;
 }
 
